f32b6d71bd691d2d70ab9947b3d53426c027d27e
Maicon de Morais <maiconmorais@alu.ufc.br>
2021-07-22 14:55:41 -0300

    Trocando vue 3 pelo vue Reorganizando arquivos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import vuetify from './plugins/vuetify'
 
-const app = createApp(App)
-app.use(router)
-app.use(store)
-app.use(vuetify)
+Vue.config.productionTip = false
 
-app.mount('#app')
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
