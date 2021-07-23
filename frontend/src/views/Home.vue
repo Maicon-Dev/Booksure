8e335d1f881f602aa2f176d3f55c3edd512e72cf
Maicon de Morais <maiconmorais@alu.ufc.br>
2021-07-23 00:10:07 -0300

    Layout escolhido

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,29 +1,79 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      class="pt-4"
-      color="grey lighten-3"
-      mini-variant
+  <v-row align="center">
+    <v-item-group
+      v-model="window"
+      class="shrink mr-6"
+      mandatory
+      tag="v-flex"
     >
-      <v-avatar
-        v-for="n in 6"
+      <v-item
+        v-for="n in length"
         :key="n"
-        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
-    </v-navigation-drawer>
+        v-slot="{ active, toggle }"
+      >
+        <div>
+          <v-btn
+            :input-value="active"
+            icon
+            @click="toggle"
+          >
+            <v-icon>mdi-record</v-icon>
+          </v-btn>
+        </div>
+      </v-item>
+    </v-item-group>
 
-    <v-main>
-      <!--  -->
-    </v-main>
-  </v-app>
+    <v-col>
+      <v-window
+        v-model="window"
+        class="elevation-1"
+        vertical
+      >
+        <v-window-item
+          v-for="n in length"
+          :key="n"
+        >
+          <v-card min-height="100vh" flat>
+            <v-card-text>
+              <v-row
+                class="mb-4"
+                align="center"
+              >
+                <v-avatar
+                  color="grey"
+                  class="mr-4"
+                ></v-avatar>
+                <strong class="text-h6">Title {{ n }}</strong>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>mdi-account</v-icon>
+                </v-btn>
+              </v-row>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
   export default {
-    //
+    data: () => ({
+      length: 3,
+      window: 0,
+    }),
   }
 </script>
